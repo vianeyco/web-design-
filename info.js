6d4095f4c4a03c3d1b12fd896f77c2646f3659c4
Vianey Castillo <vianeycastillo@Vianeys-MacBook-Pro.local>
2018-04-18 12:39:35 -0500

    thr
    trtr

---
+++ info.js
@@ -0,0 +1,6 @@
+var name = "Your Name Here";
+
+document.write("<span id ='footer'>Vianey's Website </span> </br>");
+document.write("<span id = 'footer'> &copy; 2018 Vianey Castillo </span> </br>");
+document.write("<span id = 'footer'>  Modified April 18, 2018 </span> </br>");
+document.write(Date.now())
